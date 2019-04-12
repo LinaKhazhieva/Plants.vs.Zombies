7f6029baa146640eb697b7cf8f14c33803241921
Nickolay Kudasov <nickolay.kudasov@gmail.com>
2019-04-12 15:33:30 +0300

    Add general mechanism for loading fonts

--- src/Structure/Alphabet.hs
+++ src/Structure/Alphabet.hs
@@ -2,15 +2,36 @@
 
 module Structure.Alphabet where
 
-import Graphics.Gloss
-import System.IO.Unsafe (unsafePerformIO)
+import           Data.Maybe
+import           Graphics.Gloss
+import           System.IO.Unsafe (unsafePerformIO)
 
 loadPicture :: String -> Picture
 loadPicture path = unsafePerformIO $ loadBMP path
 
+loadCharPictures :: [Char] -> Char -> Picture
+loadCharPictures cs c = fromMaybe blank (lookup c m)
+  where
+    m = zip cs (map charPicture cs)
+
+defaultCharPictures :: Char -> Picture
+defaultCharPictures = loadCharPictures (['a'..'z'] ++ ['0'..'9'])
+
+charPicture :: Char -> Picture
+charPicture c = loadPicture ("images/" ++ [c] ++ ".bmp")
+
+strPicture :: String -> Picture
+strPicture s = pictures (zipWith drawCharAt [0..] s)
+  where
+    drawCharAt i c = translate (d * fromInteger i) 0 (defaultCharPictures c)
+    d = 12
+
+numPicture :: Int -> Picture
+numPicture = strPicture . show
+
+
 a :: Picture
 a = loadPicture "images/a.bmp"
-
 
 one :: Picture
 one = pic
